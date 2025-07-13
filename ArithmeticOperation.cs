4ceffc1ec258c439ece4fb12606155277fd3fdcf
Yubraj Dhakal <112164199+Upamanyu-coder@users.noreply.github.com>
2025-07-13 22:08:47 +0545

    Create ArithmeticOperation.cs

---
+++ ArithmeticOperation.cs
@@ -0,0 +1,31 @@
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int a = 15;
+        int b = 4;
+        
+        Console.WriteLine("Addition :" + (a+b));
+        Console.WriteLine("Subtraction : " + (a-b));
+        Console.WriteLine("Multiplacation : " + (a*b));
+        Console.WriteLine("Division : " + (a/b));
+        Console.WriteLine("Modolus : " + (a%b));
+        
+    /*
+   ---------- Output ----------
+   
+      Addition :19
+      Subtraction : 11
+      Multiplacation : 60
+      Division : 3
+      Modolus : 3
+      
+      === Code Execution Successful ===
+      */
+
+
+    }
+}
