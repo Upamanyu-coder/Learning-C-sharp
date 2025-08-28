5db43803fdedb366979862a281f5d0f67113395d
Yubraj Dhakal <112164199+Upamanyu-coder@users.noreply.github.com>
2025-08-28 22:06:57 +0545

    Create Without-Return&Parameter.cs

---
+++ Without-Return&Parameter.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void SayHello()
+    {
+        Console.WriteLine("Hello, Welcome to Functions in C#!");
+    }
+
+    static void Main()
+    {
+        SayHello();   // calling function
+    }
+}
