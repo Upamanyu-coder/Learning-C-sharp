6027061565684ce138d62aa54511ca6fe3ccd5b1
Yubraj Dhakal <112164199+Upamanyu-coder@users.noreply.github.com>
2025-08-28 22:09:02 +0545

    Create Function_with_no_parameters_but_return_type.cs

---
+++ Function_with_no_parameters_but_return_type.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static string GetMessage()
+    {
+        return "C# Functions are Powerful!";
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(GetMessage());
+    }
+}
